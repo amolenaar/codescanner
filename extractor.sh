3b2073bd4b6b85bb585e8c405ea3cb2805ee2b6b
Arjan Molenaar <gaphor@gmail.com>
2021-09-29 13:40:05 +0200

    Add extractor script for git repos

---
+++ extractor.sh
@@ -0,0 +1,16 @@
+test -z "$1" && { echo "Usage: $0 <path-to-repo>"; exit 1; }
+
+cd $1
+
+git log --format=format:"::: %H %ci" --numstat | \
+while read A B C
+do
+    if test "$A" == ":::"
+    then
+        SHA="$B"
+        DATE="$C"
+    elif test "$A" != ""
+    then
+        echo "$DATE,$SHA,$A,$B,$C"
+    fi
+done
