38b37c79bac25e6bb313998e5d5e0bb1dd0d9d0b
Arjan Molenaar <gaphor@gmail.com>
2021-09-29 15:52:38 +0200

    Set max depth for git extractor

--- extractor.sh
+++ extractor.sh
@@ -2,7 +2,7 @@ test -z "$1" && { echo "Usage: $0 <path-to-repo>"; exit 1; }
 
 cd $1
 
-git log --format=format:"::: %H %ci" --numstat --no-renames | \
+git log --format=format:"::: %H %ci" --numstat --no-renames --after="two years ago" | \
 while read A B C
 do
     if test "$A" == ":::"
