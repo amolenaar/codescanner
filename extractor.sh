25ce8accdd425c25a9e0c0683ef46b4ac168f045
Arjan Molenaar <gaphor@gmail.com>
2021-10-31 16:22:39 +0100

    Fix column mismatch

--- extractor.sh
+++ extractor.sh
@@ -6,7 +6,7 @@ test -z "$1" -o -z "$2" && { echo "Usage: $0 <path-to-repo> <after>"; exit 1; }
 
 cd "$1"
 
-git log --format=format:"::: %H %ci" --numstat --no-renames --after="$2" | \
+ git log --format=format:"::: %H %ci" --numstat --no-renames --after="$2" | \
 while read A B C
 do
     if test "$A" == ":::"
